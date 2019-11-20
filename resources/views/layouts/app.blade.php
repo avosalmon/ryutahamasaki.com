8861b525a4d200196a05a9118c58cc3addf3a658
avosalmon <avosalmon@gmail.com>
2019-11-21 00:47:14 +0800

    remove unnecessary closing bracket

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        @hasSection('title'))
+        @hasSection('title')
             <title>@yield('title') - Ryuta Hamasaki</title>
         @else
             <title>Ryuta Hamasaki</title>
