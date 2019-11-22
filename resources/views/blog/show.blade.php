73ea78f628d354581fb42cc68ff268a94dea5fa5
avosalmon <avosalmon@gmail.com>
2019-11-23 00:19:11 +0800

    implement template for post page

---
+++ resources/views/blog/show.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('title', $post->title)
+
+@section('content')
+    <article>
+        <a href="/posts/{{ $post->slug }}">
+            <h1 class="text-4xl md:text-5xl font-semibold leading-tight mb-4">{{ $post->title }}</h1>
+        </a>
+        <div class="mb-10">
+            <span class="text-gray-600 mr-3">{{ $post->publish_date->format('M d, Y') }}</span>
+            @foreach ($post->tags as $tag)
+                <span class="bg-gray-300 rounded px-2 py-1 mr-1">{{ $tag->name }}</span>
+            @endforeach
+        </div>
+        <img class="object-cover mb-10" src="{{ $post->featured_image }}">
+        <div class="post-body text-xl leading-relaxed">{!! $post->body !!}</div>
+    </article>
+@endsection
