ae7a12268916f8be947a70015ca02311be5448be
avosalmon <avosalmon@gmail.com>
2019-11-23 15:24:52 +0800

    add autoprefixer

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,10 +11,12 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        require('tailwindcss')
-    ]);
+mix.js('resources/js/app.js', 'public/js');
+
+mix.postCss('resources/css/app.css', 'public/css', [
+    require('tailwindcss'),
+    require('autoprefixer')
+]);
 
 mix.copy('resources/image/avatar.jpg', 'public/image/avatar.jpg');
 
