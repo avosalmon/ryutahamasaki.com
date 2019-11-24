9c8f661b85d55c8a4e46f7e52f0c0670e181dfb8
avosalmon <avosalmon@gmail.com>
2019-11-24 13:11:56 +0800

    add alt attribute to featured image

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -4,7 +4,7 @@
     @foreach ($posts as $post)
         <article class="mb-10">
             <a class="flex flex-col md:flex-row" href="/posts/{{ $post->slug }}/">
-                <img class="object-cover mb-2 md:h-48 md:w-48 md:mr-10" src="{{ $post->featured_image }}">
+                <img class="object-cover mb-2 md:h-48 md:w-48 md:mr-10" src="{{ $post->featured_image }}" alt="{{ $post->title }}">
                 <div>
                     <h1 class="text-2xl font-semibold leading-tight mb-2">{{ $post->title }}</h1>
                     <p class="text-gray-600 mb-4">{{ $post->publish_date->format('M d, Y') }}</p>
